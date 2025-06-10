d682b6cba4f1e743a1990eb2fa2e57b253de392c
Harald Hauknes <harald@hauknes.org>
2025-06-10 09:50:15 +0200

    this file is far from safe or complete

--- deploy.ps1
+++ deploy.ps1
@@ -2,19 +2,19 @@
 # There's an interesting group policy to change that, but thats for later
 # TODO: Since propably running as administrator, use runas or similar for the correct user
 # Administrator shell
-echo "Skipping package-insllations - you can find the commands in the script"
-#winget install --id Git.Git -e --source winget
-#winget install Microsoft.VisualStudioCode
-#winget install --id Microsoft.PowerToys --source winget
-#winget install --id=M2Team.NanaZip -e
-
-# TODO PowerToys, Joplin
+echo "Skipping package-installations - you can find the commands in the script"
+# winget install --id Git.Git -e --source winget
+# winget install Microsoft.VisualStudioCode
+# winget install --id Microsoft.PowerToys --source winget
+# winget install --id=M2Team.NanaZip -e
+# Joplin
 # Normal shell
-ssh-keygen
-Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-git submodule init
-git submodule update --remote
-# New-Item -Path ([System.Environment]::GetFolderPath('MyDocuments') + "\WindowsPowerShell\profile.ps1") -ItemType SymbolicLink -Value ($HOME + "\.config\dotfiles\ps\profile.ps1")
+# TODO check if SSH-key exists 
+# ssh-keygen
+#Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+#git submodule init
+#git submodule update --remote
+# New-Item -Path ([System.Environment]:GetFolderPath('MyDocuments') + "\WindowsPowerShell\profile.ps1") -ItemType SymbolicLink -Value ($HOME + "\.config\dotfiles\ps\profile.ps1")
 # If documents folder is not in default locations, powershell can't find the profile
 # The $HOME-variable does not change from original user in elevated promt
 New-Item -Path ($PSHOME + "\Profile.ps1") -ItemType SymbolicLink -Value ($HOME + "\.config\dotfiles\ps\profile.ps1")
