4fd9cd8ea503a9f9f4a612f7244567a6b58d21d5
Harald Hauknes <harald@hauknes.org>
2026-03-29 16:45:06 +0200

    Less lazy checks and more verbose output

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,93 @@
-#/bin/env sh
+#/bin/bash
+
+##
+# Quick onboarding for new clients, symlinks configuration-files to this repo
+##
+#TODO: .zshrc, Joplin
+
+## Repository ##
 DIR=(`pwd`)
-echo Configuring homedir with dotfiles from $DIR
-echo Fetching submodules..
+echo "Dotfiles will be sourced from '$DIR'"
+echo Git: Fetching submodules in the 'dotfiles'-repo..
 git submodule init
 git submodule update --remote
+## Applications ##
 cd
-echo Configuring vim..
-ln -s $DIR/.vim
-ln -s $DIR/.vimrc
-echo Configuring i3
-mv .config/i3 .config/i3.backup
-ln -s $DIR/i3 .config/i3
-echo Settings for GNU Screen
-ln -s $DIR/.screenrc .screenrc
-echo Symlinking .XResources
-ln -s $DIR/.Xresources .Xresources
-echo Sourcing powerline to bash
-echo source $DIR/bash/bash-powerline/bash-powerline.sh >> .bashrc
-# Since i don't bother with a bashrc for most systems now
-echo export TERM=xterm-256color >> ~/.bashrc
-echo Done.
-
+# TODO: Write a function to reuse the tests
+echo Configuring Vim..
+if [ -L .vim ]; then
+  echo "'.vim' was already symlinked, skipping Vim"
+else
+  ln -s $DIR/.vim $HOME/.vim
+  ln -s $DIR/.vimrc $HOME/.vimrc
+  echo "Symlinked '.vim' and '.vimrc'. Use ':BundleInstall!' in the editor if still using 'Vundle'."
+fi
+echo "Configuring 'i3'.."
+if [ -e "$HOME/i3" ]; then
+  echo "Old 'i3'-config found directly in home-directory, will be moved to '.config/i3.backup_`date -I`"
+  mv $HOME/i3 $HOME/.config/i3.backup_`date -I`
+fi
+if [ -e "$HOME/.config/i3" ]; then
+  if [ -e "$HOME/.config/i3" ]; then
+    if [ -L "$HOME/.config/i3" ]; then
+      echo "'$HOME/.config/i3' already symlinked, skipping."
+    else
+      echo "Existing 'i3'-config moved to '.config/i3/backup_`date -I`'"
+      mv $HOME/.config/i3 $HOME/.config/i3.backup_`date -I`
+      ln -s $DIR/i3 $HOME/.config/i3
+      echo "Symlinked 'i3'-configuration."
+    fi
+  fi
+else
+  ln -s $DIR/i3 $HOME/.config/i3
+  echo "Symlinked 'i3'-configuration."
+fi
+echo "Settings for 'GNU Screen'.."
+if [ -f "$HOME/.screenrc" ]; then
+  if [ -L "$HOME/.screenrc" ]; then
+    echo "'.screenrc' already symlinked. Skipping.."
+  else
+    echo "Existing '.screenrc' will be moved to $HOME/.config/.screenrc_backup_`date -I`"
+    mv $HOME/.screenrc $HOME/.config/.screenrc_backup_`date -I`
+    ln -s $DIR/.screenrc $HOME/.screenrc
+    echo "'GNU Screen' configured."
+  fi
+else
+  ln -s $DIR/.screenrc $HOME/.screenrc
+  echo "'.screenrc' now symlinked."
+fi
+if [ -e "/Users/" ]; then
+  echo "Skipping '.XResources' as system is likely 'Mac OS' or 'MS Windows' (/Users exist)"
+else
+  echo "Symlinking '.XResources'"
+  ln -s $DIR/.Xresources $HOME/.Xresources
+fi
+## Bash ##
+if [ -L "$HOME/.bashrc" ]; then
+  echo "'.bashrc' already symlinked. Skipping."
+else
+  if [ -f "$HOME/.bashrc" ]; then
+    echo "Existing '.bashrc' will be moved to '.config/.bashrc_backup_`date -I`"
+    mv $HOME/.bashrc $HOME/.config/.bashrc_backup_`date -I`
+    ln -s $DIR/bash/.bashrc $HOME/.bashrc
+    echo "'.bashrc' now symlinked."
+  else
+    ln -s $DIR/bash/.bashrc $HOME/.bashrc
+    echo "'.bashrc' now symlinked."
+  fi
+fi
+if [ -L "$HOME/.bash_profile" ]; then
+  echo "'.bash_profile' already symlinked. Skipping."
+else
+  if [ -f ".bash_profile" ]; then
+    echo "Existing '.bash_profile' will be moved to '.config/.bash_profile_`date -I`"
+    mv $HOME/.bash_profile $HOME/.config/.bashrc_profile_`date -I`
+    ln -s $DIR/bash/.bash_profile $HOME/.bash_profile
+    echo "'.bash_profile' symlinked."
+  else
+    ln -s $DIR/bash/.bash_profile $HOME/.bash_profile
+    echo "'.bash_profile' symlinked."
+  fi
+fi
+# All done
+echo "'deploy.sh' Done."
