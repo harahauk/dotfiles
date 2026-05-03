c15456e8f139819944100a3d5efda26864c7362b
Harald Hauknes <harald@hauknes.org>
2026-05-03 15:30:09 +0200

    conditional un-pack if system supports it

--- windows/install_rsync_from_msys.ps1
+++ windows/install_rsync_from_msys.ps1
@@ -9,7 +9,7 @@
 ##
 
 ## Defaults ##
-$installation_path = $HOME + "/.local_test"
+$installation_path = $HOME + "/.local" # Add <this>/usr/bin to PATH
 $baseUrl = "https://repo.msys2.org/msys/x86_64/"
 $temp_storage = "./temp_rsync_installation"
 $filename_trail = "-x86_64.pkg.tar.zst"
@@ -28,14 +28,14 @@ foreach ($package_name in $rsync_dependencies) {
     $url = $baseUrl + $package_name + $filename_trail
     $dest = $temp_storage + '/' + $package_name + $filename_trail
     Write-Host -ForegroundColor DarkCyan "$package_name$filename_trail"
+    #TODO: Don't download if file already exist
     Invoke-WebRequest -Uri $url -OutFile $dest
 }
 #
 ## Unarchiving ##
-# TODO: Check for Windows built-in features
+# TODO: Alternately check for 7-zip/Nanazip
 $tar_capable = $(where.exe tar.exe)
-Write-Host $tar_capable
-if ( $true ) {
+if ( $tar_capable.Length -gt 5 ) {
   Write-Host "Un-packing downloaded tarballs:"
   foreach ($package_name in $rsync_dependencies) {
     $url = $baseUrl + $package_name + $filename_trail
